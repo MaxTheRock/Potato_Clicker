1ac1ecc0770057bc7a1ba75759b7506db6966df7
MaxTheRock <maxtherock@proton.me>
2026-02-03 21:52:16 +0000

    removed code for arthur

--- script.js
+++ script.js
@@ -1858,9 +1858,6 @@
       Codeinput.value = "";
       alert("Code redeemed! Achievement unlocked: How did you do that!");
     }
-    if (value === "pooonmyface") {
-      allTimePotatoes = 1250000000000
-    }
   });
 
   function storeCounter() {
