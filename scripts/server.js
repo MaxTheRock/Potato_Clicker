5a4f9047a7eb5e428c962f820bf7d1af3d4b036d
MaxTheRock <maxtherock@proton.me>
2026-02-22 17:34:49 +0000

    dfghfsghsfgh

--- scripts/server.js
+++ scripts/server.js
@@ -27,10 +27,9 @@ const maintenance = process.env.MAINTENANCE_MODE === "true";
 
 if (maintenance) {
   app.use((req, res) => {
-    res.status(503).sendFile(__dirname + "/maintenance.html");
+    res.status(503).sendFile(path.join(process.cwd(), "maintenance.html"));
   });
 }
-
 
 /* -------------------------------------------------------------
    Database schema – users, saves, leaderboard
